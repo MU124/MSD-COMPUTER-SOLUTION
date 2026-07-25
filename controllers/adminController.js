d15c05fd5d96d4e8458f66a0c411073eb6f75701
Mayur Utekar <mayur@digitalenterprises.co.in>
2026-07-25 17:17:57 +0530

    Switch to pure JavaScript bcryptjs to prevent ERR_DLOPEN_FAILED on Windows hosting

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const AdminModel = require('../models/adminModel');
 const ProductModel = require('../models/productModel');
 const CategoryModel = require('../models/categoryModel');
